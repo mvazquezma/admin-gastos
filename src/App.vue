254a48ed3b4efb010595019b5b814ded4a1c8c59
mvazquezma <mvm51090@gmail.com>
2023-07-04 17:49:44 +0200

    Format date helper, added spent list, images and styles

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
   import Presupuesto from './components/Presupuesto.vue';
   import ControlPresupuesto from './components/ControlPresupuesto.vue';
   import Modal from './components/Modal.vue'
+  import Gasto from './components/Gasto.vue'
   import { generarId } from './helpers'
   import iconoNuevoGasto from './assets/img/nuevo-gasto.svg'
 
@@ -79,6 +80,16 @@
       </div>
     </header>
     <main v-if="presupuesto > 0">
+
+      <div class="listado-gastos contenedor">
+        <h2>{{ gastos.length > 0 ? 'Gastos' : 'No hay gastos'}}</h2>
+
+        <Gasto
+          v-for="gasto in gastos"
+          :key="gasto.id"
+          :gasto="gasto"
+        />
+      </div>
 
       <div class="crear-gasto">
         <img 
@@ -163,5 +174,13 @@
   .crear-gasto img {
     width: 5rem;
     cursor: pointer;
+  }
+
+  .listado-gastos {
+    margin-top: 10rem;
+  }
+  .listado-gastos h2 {
+    font-weight: 900;
+    color: var(--gris-oscuro);
   }
 </style>
