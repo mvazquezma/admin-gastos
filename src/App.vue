53a4076f8dbb80cb7eb758f2a90c32181b1a4b2e
mvazquezma <mvm51090@gmail.com>
2023-07-05 19:13:48 +0200

    spent to localstorage

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-  import { ref, reactive, watch, computed } from 'vue';
+  import { ref, reactive, watch, computed, onMounted } from 'vue';
   import Presupuesto from './components/Presupuesto.vue';
   import ControlPresupuesto from './components/ControlPresupuesto.vue';
   import Modal from './components/Modal.vue'
@@ -31,6 +31,7 @@
     const totalGastado = gastos.value.reduce((total, gasto) => gasto.cantidad + total, 0)
     gastado.value = totalGastado
     disponible.value = presupuesto.value - totalGastado
+    localStorage.setItem('gastos', JSON.stringify(gastos.value))
   }, {
     deep: true
   })
@@ -41,6 +42,23 @@
     }
   }, {
     deep: true
+  })
+
+  watch(presupuesto, () => {
+    localStorage.setItem('presupuesto', presupuesto.value)
+  })
+
+  onMounted(() => {
+    const presupuestoStorage = localStorage.getItem('presupuesto')
+    if(presupuestoStorage) {
+      presupuesto.value = Number(presupuestoStorage)
+      disponible.value = Number(presupuestoStorage)
+    }
+
+    const gastosStorage = localStorage.getItem('gastos')
+    if(gastosStorage) {
+      gastos.value = JSON.parse(gastosStorage)
+    }
   })
 
   const definirPresupuesto = (cantidad) => {
